98c7c1fa5c522b2c5b8c6f40f31fbe14cda3bc25
jbfaden <faden@cottagesystems.com>
2020-01-15 10:07:28 -0600

    create master index

--- src/tips/DocletTip.java
+++ src/tips/DocletTip.java
@@ -10,13 +10,16 @@ import com.sun.javadoc.SeeTag;
 import com.sun.javadoc.Tag;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.regex.Pattern;
 
 /**
  * From https://www.zdnet.com/article/customize-javadoc-output-with-doclets/
@@ -27,7 +30,7 @@ import java.util.regex.Pattern;
  */
 public class DocletTip {
 
-    private static final File apdoc= new File("/tmp/apdoc/");
+    private static final File apdoc= new File("/home/jbf/tmp/autoplot/2020/20200115_apdoc/");
     
     
     /**
@@ -95,11 +98,14 @@ public class DocletTip {
             if ( !apdoc.mkdirs() ) throw new IllegalStateException("can't make dir: "+apdoc);
         }
         
-        for (int i = 0; i < classes.length; i++) {
+        Map<String,String> grandIndex= new HashMap<>();
+        Map<String,String> grandIndexFirst= new HashMap<>();
+        
+        for (ClassDoc classe : classes) {
             
             PrintStream out= null;
             try {
-                String s= classes[i].qualifiedName();
+                String s = classe.qualifiedName();
                 System.err.println("# "+s);
                 int is= 0;
                 for ( int j=0; j<s.length(); j++ ) {
@@ -113,8 +119,19 @@ public class DocletTip {
                     s= s.substring(0,is).replaceAll("\\.","/") + s.substring(is);
                 } else {
                     throw new IllegalStateException("didn't find upper case letter");
-                }   
-                File f= new File( apdoc.toString() + "/" + s + ".md" );
+                }
+                MethodDoc[] methods = classe.methods();                
+                Arrays.sort( methods, (MethodDoc o1, MethodDoc o2) -> o1.name().compareTo(o2.name()) );
+                int nmethod= methods.length;
+                boolean byAlpha= false;
+                char currentLetter= 'a';
+                File f; // the current file to which we are writing
+                if ( nmethod>200 ) {
+                    byAlpha= true;
+                    f= new File( apdoc.toString() + "/" + s + "_" + currentLetter + ".md" );
+                } else {
+                    f= new File( apdoc.toString() + "/" + s + ".md" );
+                }
                 File d= f.getParentFile();
                 if ( !d.exists() ) {
                     if ( !d.mkdirs() ) throw new IllegalStateException("can't make dir: "+d);
@@ -123,18 +140,24 @@ public class DocletTip {
                 if ( !f.getParentFile().exists() ) {
                     if ( !f.getParentFile().mkdirs() ) throw new IllegalStateException("can't make dir");
                 }
-                
-                MethodDoc methods[] = classes[i].methods();
-                
-                Arrays.sort( methods, (MethodDoc o1, MethodDoc o2) -> o1.name().compareTo(o2.name()) );
-                
-                int nmethod= methods.length;
-                for (int j = 0; j < Math.min( 200, nmethod ); j++) {
+                for (int j = 0; j < Math.min( 20000, nmethod ); j++) {
                     MethodDoc m= methods[j];
+                    
+                    String name= m.name();
+                    
+                    if ( byAlpha ) {
+                        if ( name.charAt(0)!=currentLetter ) {
+                            out.close();
+                            currentLetter= name.charAt(0);
+                            f= new File( apdoc.toString() + "/" + s + "_"+ currentLetter + ".md" );
+                            out = new PrintStream(f);
+                        }
+                    }
+                    
                     StringBuilder sb= new StringBuilder();
                     StringBuilder ahrefBuilder= new StringBuilder();
-                    sb.append(m.name()).append("( ");
-                    ahrefBuilder.append(m.name()).append("(");
+                    sb.append(name).append("( ");
+                    ahrefBuilder.append(name).append("(");
                     for ( int k=0; k<m.parameters().length; k++ ) {
                         if ( k>0 ) sb.append(", ");
                         if ( k>0 ) ahrefBuilder.append(",");
@@ -153,7 +176,7 @@ public class DocletTip {
                     }
                     out.println("***");
                     out.println("<a name=\""+ahrefBuilder.toString()+"\"></a>");
-                    out.println("# "+m.name());
+                    out.println("# "+name);
                     out.println(sb.toString());
                     out.println("");
                     out.println(m.commentText());
@@ -183,8 +206,8 @@ public class DocletTip {
                     if ( seeTags.length>0 ) {
                         out.println("### See Also:");
                     }
-                    for ( int k=0; k<seeTags.length; k++ ) {
-                        SeeTag t= (SeeTag)seeTags[k];
+                    for (Tag seeTag : seeTags) {
+                        SeeTag t = (SeeTag) seeTag;
                         int it= t.text().indexOf(')');
                         String l;
                         if ( it>-1 ) {
@@ -198,14 +221,31 @@ public class DocletTip {
                             out.println("<a href='"+l+"'>" + seeAlsoLabel(l) +"</a> "+t.label()+"<br>" );
                         }
                     }
-                    out.println( String.format( "\n<a href=\"https://github.com/autoplot/dev/search?q=%s&unscoped_q=%s\">search for examples</a>", m.name(), m.name() ) );
-                    
+                    out.println( String.format( "\n<a href=\"https://github.com/autoplot/dev/search?q=%s&unscoped_q=%s\">search for examples</a>", name, name ) );
+                    grandIndex.put( name, s + "_"+ currentLetter + ".md#"+name );
+                    String firstSentence= m.commentText();
+                    grandIndexFirst.put( name, firstSentence.substring(0,Math.min(60,firstSentence.length()) ) );
                 }
-            } catch (FileNotFoundException ex) {
+            }catch (FileNotFoundException ex) {
                 Logger.getLogger(DocletTip.class.getName()).log(Level.SEVERE, null, ex);
             } finally {
                 out.close();
             }
+        }
+        
+        File grandIndexFile= new File( apdoc.toString() + "/index-all.md" );
+        try ( PrintStream indexOut=new PrintStream(grandIndexFile) ) {
+            List<String> keys= new ArrayList( grandIndex.keySet() );
+            Collections.sort(keys);
+            for ( String k: keys ) {
+                indexOut.print("<a href=\""+grandIndex.get(k)+"\">");
+                indexOut.print(k);
+                indexOut.print("</a>");
+                indexOut.print(grandIndexFirst.get(k));
+                indexOut.println("<br>");
+            }
+        } catch ( IOException out ) {
+            throw new IllegalStateException("could not write to "+grandIndexFile);
         }
 
         return true;
